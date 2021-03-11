dcaaa406ddf0db7ccc72501a30b6c1d6d1c05f0d
MarcosPauloSantosGomes <marcospaulosantosgomes@gmail.com>
2021-03-11 16:47:34 -0300

    Backend com Update e Delete

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -50,3 +50,52 @@ exports.SelectDetail = (req, res, next) => {
         })
         .catch(error => next(error));
 };
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const dataNascimento = req.body.dataNascimento;
+    const ativo = req.body.ativo;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.update({
+                    nome: nome,
+                    dataNascimento: dataNascimento,
+                    ativo: ativo
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+    };
